d9a7207a1dd9a81d7412453519d9df48441c8de0
Dang Van Phu <phudang24012000@gmail.com>
2023-07-31 11:00:43 +0700

    Add project files.

---
+++ Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Models;
+using ShopNuocHoa.Areas.Admin.Code;
+using ShopNuocHoa.Areas.Admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopNuocHoa.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(LoginModel model)
+        {
+            var result = new AccountModels().Login(model.Username, model.Password);
+            if (result && ModelState.IsValid)
+            {
+                SessionHelper.SetSession(new UserSession() { Username = model.Username });
+                return RedirectToAction("Index", "SanPhams");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Tên đăng nhập hoặc mật khẩu không đúng !!!");
+            }
+            return View(model);
+        }
+    }
+}
